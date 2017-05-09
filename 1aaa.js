80f56a530024722a8d5e2b4e187ee5d5c72f832f
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-05-09 11:13:07 -0500

    Create 1aaa.js

---
+++ 1aaa.js
@@ -0,0 +1,15 @@
+function add(x, y){
+	document.write(+x + +y);
+}
+function sub(x, y){
+	document.write(x - y);
+}
+function mul(x, y){
+	document.write(x * y);
+}
+function div(x, y){
+	document.write(x / y);
+}
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2nd Number");
+var oper = prompt("Enter Operation");
